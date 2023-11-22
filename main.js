491a94f0cc2c4482f90d496bf85e1148df790a1f
Shibin S P <shibinsp45@gmail.com>
2023-11-22 22:30:26 +0530

    new files added

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-const getTimeData = () => {
-    const localTime = new Date();   
-    const hours =localTime.getHours()
-    const minutes =localTime.getMinutes()
-    const seconds =localTime.getSeconds()
-
-    const displayTime=hours>=12? `${hours}:${minutes}:${seconds} PM` : `${hours}:$
-    {minutes}:${seconds} AM`
-    result.innerHTML=displayTime
-    setTimeout(()=>{
-        getTimeData()
-    },1000)
+  function clock(){
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let formatHours = convertFormat(hours);
+        hours = checkTime(hours);
+        hours = addZero(hours);
+        minutes = addZero(minutes);
+        seconds = addZero(seconds);
+        document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds} ${formatHours}`
     }
-    getTimeData()
+    function convertFormat(time){
+        let format = 'AM';
+        if(time >= 12){
+            format = 'PM';
+        }
+        return format;
+    }
+    function checkTime(time){
+        if(time > 12){
+            time = time - 12;
+        }
+        if(time === 0){
+            time = 12;
+        }
+        return time;
+    }
+    function addZero(time){
+        if(time < 10){
+            time = '0' + time;
+        }
+        return time;
+    }
+    clock();
+    setInterval(clock, 1000);
